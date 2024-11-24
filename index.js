82efcf656a43fecef22b4847a292b9538e016eb2
boluwaatife ajijola <ajijolanurudeen@gmail.com>
2024-11-24 12:13:08 +0100

    la

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const cors = require("cors");
 
 const bodyParser = require("body-parser");
 
-const controllers = require("../backend/src/controllers/quotes")
+const axios = require("axios")
+
+//const controllers = require("../backend/src/controllers/quotes")
 
 const app = express();
+
+app.use(express.json())
 
 require("dotenv").config();
 
@@ -14,13 +18,40 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
-//Routes
-app.get("/quote", controllers.getRandomQoute)
-app.post("/quote",controllers.saveFavoriteQuote)
-
 app.all("/", (req, res) => {
     res.status(200).json({ message: "Hello from serverless function!" });
 });
+//Routes
+
+let favoriteQuotes = []
+
+//Get random quote
+const getRandomQoute = async (req,res)=>{
+    try{
+        const response = await axios.get("http://api.quotable.io/random");
+        res.status(200).json(response.data);
+    }catch(error){
+        console.error("Error fetching quote:", error); // Log the error for debugging
+        res.status(500).json({error:"cannot fetch quote", details: error.message});
+    }
+};
+
+// favorite quote
+const saveFavoriteQuote = async(req,res)=>{
+    const{quote,author}=req.body;
+
+    if (!quote || !author){
+        return res.status(400).json({error:"Author and Quote required"})
+    }
+
+    favoriteQuotes.push({quote,author});
+
+    res.json({favoriteQuotes});
+    
+};
+app.get("/quote", getRandomQoute)
+app.post("/quote",saveFavoriteQuote)
+
 module.exports = (req, res) => {
     // This wraps your Express app into a serverless function
     app(req, res);
