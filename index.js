a62177ee139110d8cfb5346671195ae2be1b85d6
boluwaatife ajijola <ajijolanurudeen@gmail.com>
2024-11-24 07:27:23 +0100

    get

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ app.use(bodyParser.json());
 app.get("/quote", controllers.getRandomQoute)
 app.post("/quote",controllers.saveFavoriteQuote)
 
+app.all("/", (req, res) => {
+    res.status(200).json({ message: "Hello from serverless function!" });
+});
+module.exports = (req, res) => {
+    // This wraps your Express app into a serverless function
+    app(req, res);
+};
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT,()=> console.log(`server running on port ${PORT}`))
